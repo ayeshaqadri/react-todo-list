4d00bdffa27336b2bb6aff277cc3ed71ad7cea5d
ayeshaqadri <qadria392@gmail.com>
2025-02-03 21:18:46 +0500

    'incomplete'

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 // function App() {
-
 //   return (
 //     <>
-
 // <h1>Ayesha</h1>
 // <p>To-Do list</p>
 //     </>
 //   )
 // }
-
 // export default App
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
